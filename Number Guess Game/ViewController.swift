688afbc1ff2fa07f0cbb987dcf37aee971e8182e
kingct19 <kingchandler19@gmail.com>
2023-06-16 11:26:24 -0500

    Initial Commit

---
+++ Number Guess Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Number Guess Game
+//
+//  Created by Chandler King on 6/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
